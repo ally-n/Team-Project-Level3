a2facf3e0cff668c4ab9b9d6ceb5abdb649b78f8
kellyramos31 <72636939+kellyramos31@users.noreply.github.com>
2021-05-29 19:11:35 -0600

    checking

--- capstone-level3/src/RandomMeme.js
+++ capstone-level3/src/RandomMeme.js
@@ -5,7 +5,7 @@ function RandomMeme(props) {
     console.log(props)
     return (
         <div>
-            <img width="250px" height="250px" src={props.url} alt="meme-photo" />
+            <img width="250px" height="250px" src={props.url} alt="meme" />
         </div>
     )
 }
